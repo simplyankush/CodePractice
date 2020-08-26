c0d0f014c8034ec77ce85f4ea263cafce4984a7c
Ankush Grover <simplyankush@gmail.com>
2020-08-27 02:37:01 +0530

    new work

---
+++ DataStructures/DataStructures/CheckCompletenessOfBinaryTree.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    class CheckCompletenessOfBinaryTree
+    {
+        /**
+         * 
+         * Given a binary tree, determine if it is a complete binary tree.
+
+Definition of a complete binary tree from Wikipedia:
+In a complete binary tree every level, except possibly the last, is completely filled, 
+        and all nodes in the last level are as far left as possible.
+        It can have between 1 and 2h nodes inclusive at the last level h.
+
+        Apply BFS
+        For every node, add it to the queue
+        Pop the queue and add its left and right , until node popped is a null
+
+        keep popping the queue till the first element is null;
+
+        if at the end , the queue is empty, it is a complete binary tree.
+
+         * */
+        public bool IsCompleteTree(TreeNode root)
+        {
+            var queue = new Queue<TreeNode>();
+
+            queue.Enqueue(root);
+
+            while (queue.Count > 0 && queue.Peek() != null)
+            {
+                var element = queue.Dequeue();
+                queue.Enqueue(element.left);
+                queue.Enqueue(element.right);
+            }
+
+
+            while (queue.Count > 0 && queue.Peek() == null)
+            {
+                queue.Dequeue();
+            }
+
+            return queue.Count == 0;
+        }
+    }
+}
---
+++ DataStructures/DataStructures/CriticalConnections1192.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    public class CriticalConnections1192
+    {
+        bool[] visited;
+        List<List<int>> adjList;
+        int[] identifiers;
+        int[] lowValues;
+        int count = 0;
+
+        public IList<IList<int>> CriticalConnections(int n, IList<IList<int>> connections)
+        {
+            visited = new bool[n];
+            identifiers = new int[n];
+            lowValues = new int[n];
+
+            adjList = new List<List<int>>();
+            for (int i = 0; i < n; i++)
+            {
+                adjList.Add(new List<int>());
+            }
+
+            var result = new List<IList<int>>();
+
+            foreach (var conn in connections)
+            {
+                var a = conn[0];
+                var b = conn[1];
+
+                adjList[a].Add(b);
+                adjList[b].Add(a);
+            }
+
+            dfs(0, -1);
+
+            for (int i = 0; i < n; i++)
+            {
+                foreach (var neighbor in adjList[i])
+                {
+                    if (identifiers[i] < lowValues[neighbor])
+                    {
+                        result.Add(new List<int>() { i, neighbor });
+                    }
+                }
+            }
+
+
+            return result;
+
+        }
+
+        public void dfs(int current, int previous)
+        {
+            visited[current] = true;
+            identifiers[current] = count;
+            lowValues[current] = count;
+            count++;
+
+            var neighbors = adjList[current];
+
+            foreach (var n in neighbors)
+            {
+                if (n == previous)
+                {
+                    continue;
+                }
+
+                if (!visited[n])
+                {
+                    dfs(n, current);
+                }
+
+
+                lowValues[current] = Math.Min(lowValues[current], lowValues[n]);
+            }
+        }
+    }
+}
